2d61587e6c3e58bf541652f1e0247f1832bb255b
Niko <artwarchol@gmail.com>
2018-12-17 12:22:41 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :models
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root to: "home#index"
+
+  # <p class="notice"><%= notice %></p>
+  #      <p class="alert"><%= alert %></p>
+end
