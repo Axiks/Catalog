819de5843edf93a7e4d99ead856d3846d22c7f3b
Niko <artwarchol@gmail.com>
2018-12-17 12:48:23 +0200

    add product

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
   get 'users/index'
   get 'users/show'
   devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root to: "home#index"
+  #
 
   # <p class="notice"><%= notice %></p>
   #      <p class="alert"><%= alert %></p>
