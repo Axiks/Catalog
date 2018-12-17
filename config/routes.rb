b559bc1bc34afbb323e13640549b2e7799dc262e
Niko <artwarchol@gmail.com>
2018-12-17 13:27:00 +0200

    Цього разу щось і написав |

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
+
   get 'users/index'
   get 'users/show'
   devise_for :models
+  #devise_for :users
+  resources :products
+
+  root to: "products#index"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
 
